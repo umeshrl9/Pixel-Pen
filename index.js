8b92175f6e7b62591f90f8cab224a982fb1cc401
Umesh Kashyap <umesh.kashyap3729@gmail.com>
2024-12-22 21:14:40 +0530

    added basic header and file structure changes

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const app = express();
 const port = 3000;
