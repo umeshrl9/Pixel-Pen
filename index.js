4a63d89cc7e33fae1ba05bc1efb00cc58553bb71
Umesh Kashyap <umesh.kashyap3729@gmail.com>
2024-12-27 21:10:20 +0530

    Finalized the header and titlebar

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+app.use(express.static("public"));  
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
