2c8c36754d672e1374ba2ef2b04b1be26bd5a019
Umesh Kashyap <umesh.kashyap3729@gmail.com>
2025-01-01 21:13:15 +0530

    functionality for creating a blog added

--- index.js
+++ index.js
@@ -2,20 +2,40 @@ import express from "express";
 import bodyParser from "body-parser";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));  
+app.use(bodyParser.urlencoded({extended: true}));
+
+var blogs = [];
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
 app.get("/main", (req, res) => {
-    res.render("main.ejs");
+    res.render("main.ejs", {
+        blogTitles: blogTitlesArray
+    });
 })
+
+app.get("/create", (req, res) => {
+    res.render("createBlog.ejs");
+});
+
+app.post('/create', (req, res) => {
+    const newBlog = {
+        id: Date.now(), // Unique identifier
+        title: req.body.title,
+        content: req.body.content,
+    };
+    blogs.push(newBlog);
+    res.redirect('/'); 
+    console.log(blogs);
+});
 
 app.listen(port, () => {
     console.log("Server running on port: " + port); 
