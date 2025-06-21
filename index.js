4e5a8ea23509f44b8d404e6b3f764f0231056de7
Umesh Kashyap <umesh.kashyap3729@gmail.com>
2025-06-21 13:55:53 +0530

    added secure login system and integrated postgres, /main and /create features added

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-var blogs = [];
+import pg from "pg";
+import dotenv from "dotenv";
+import session from "express-session";
+import bcrypt from "bcryptjs";
 
 const app = express();
 const port = 3000;
+dotenv.config();
+
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+        maxAge: 24 * 60 * 60 * 1000
+    }
+}))
+
+const { Pool } = pg;
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 app.use(express.static("public"));  
 app.use(bodyParser.urlencoded({extended: true}));
@@ -19,24 +41,115 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
-app.get("/main", (req, res) => {
-    res.render("main.ejs", {
-        blogs: blogs
-    });
-})
+app.get("/main", async (req, res) => {
+    if(!req.session.userID){
+        return res.redirect("/auth");
+    }
+
+    try{
+        const result = await pool.query(
+            "SELECT * FROM blogs WHERE user_id = $1 ORDER BY id DESC",
+            [req.session.userID]
+        );
+
+        res.render("main.ejs", {
+            blogs: result.rows
+        });
+    } catch (err){
+        console.log(error);
+        res.status(500).send("Error fetching blogs");
+    }   
+});
 
 app.get("/create", (req, res) => {
+    if(!req.session.userID){
+        return res.redirect("/auth");
+    }
+
     res.render("createBlog.ejs");
 });
 
-app.post('/create', (req, res) => {
-    const newBlog = {
-        id: Date.now(), // Unique identifier
-        title: req.body.title,
-        content: req.body.content,
-    };
-    blogs.push(newBlog);
-    res.redirect('/main'); 
+app.get("/auth", (req, res) => {
+    if(!req.session.userID){
+        res.render("auth.ejs");
+    } else{
+        res.redirect("/main");
+    }
+});
+
+app.post("/register", async (req, res) => {
+    const { username, password } = req.body;
+
+    try{
+        const existingUser = await pool.query(
+            "SELECT * FROM users WHERE username = $1", 
+            [username]
+        );
+
+        if(existingUser.rows.length > 0){
+            return res.send("Username already taken");
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const result = await pool.query(
+            "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING ID",
+            [username, hashedPassword]
+        );
+
+        req.session.userID = result.rows[0].id;
+        res.redirect("/main");
+    } catch (err){
+        console.log(err);
+        res.redirect("/auth");
+    }
+});
+
+app.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+
+    try{
+        const result = await pool.query(
+            "SELECT * FROM users WHERE username LIKE $1",
+            [username]
+        );
+
+        if(result.rows.length > 0){
+            const storedHash = result.rows[0].password;
+            const isMatch = await bcrypt.compare(password, storedHash);
+
+            if(isMatch){
+                req.session.userID = result.rows[0].id;
+                res.redirect("/main");
+            } else{
+                return res.send("Wrong Password");
+            }
+        } else{
+            return res.send("No account exists with the given username");
+        }
+    } catch (err) {
+        console.log(err);
+        res.redirect("/auth");
+    }
+})
+
+app.post('/create', async (req, res) => {
+    if(!req.session.userID){
+        return res.redirect("/auth");
+    }
+
+    const { title, content } = req.body;
+
+    try{
+        await pool.query(
+            "INSERT INTO blogs (title, content, user_id) VALUES ($1, $2, $3)",
+            [title, content, req.session.userID]
+        );
+
+        res.redirect("/main");
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Error creating blog");
+    }
 });
 
 app.get('/blogs/:id', (req, res) => {
---
+++ queries.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE blogs (
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  content TEXT NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
